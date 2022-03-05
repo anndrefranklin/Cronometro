2e8801025b0de1566add9c42651e31ba1455b711
RaioDigital <raiodigitalweb@gmail.com>
2022-03-04 23:20:29 -0300

    Implantando nova imagem do app

--- src/App.js
+++ src/App.js
@@ -1,63 +1,75 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
-import './estilo.css'
+import "./estilo.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: "Iniciar",
+    };
 
-constructor(props){
-  super(props);
-  this.state = {
-    numero:0,
-    botao: 'Iniciar'
-  };
-
-  this.timer = null;
-  this.iniciar = this.iniciar.bind(this);
-  this.limpar = this.limpar.bind(this);
-}
-
-iniciar(){
-  let state = this.state;
-
-  if(this.timer !== null){
-    clearInterval(this.timer);
     this.timer = null;
-    state.botao = 'Iniciar';
-  }else{
-    this.timer = setInterval(()=>{
-      let state = this.state;
-      state.numero += 0.1;
-      this.setState(state);
-    },100);
-    state.botao = 'Pausar';
-  } 
-
-  this.setState(state);
-}
-
-limpar(){
-  if(this.timer !== null){
-    clearInterval(this.timer);
-    this.timer = null;
+    this.iniciar = this.iniciar.bind(this);
+    this.limpar = this.limpar.bind(this);
   }
 
-  let state = this.state;
-  state.numero = 0;
-  state.botao = 'Iniciar';
-  this.setState(state);
-}
+  iniciar() {
+    let state = this.state;
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = "Iniciar";
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      }, 100);
+      state.botao = "Pausar";
+    }
+
+    this.setState(state);
+  }
+
+  limpar() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = "Iniciar";
+    this.setState(state);
+  }
 
   render() {
     return (
       <div className="container">
-        <img src={require('../src/assets/cronometro.png')} className="img" alt="img" />        
+        <img
+          src={require("../src/assets/cronometro.png")}
+          className="img"
+          alt="img"
+        />
         <a className="timer">{this.state.numero.toFixed(1)}</a>
-        
+
         <div className="areaBtn">
-          <a className="botao" onClick={this.iniciar}>{this.state.botao}</a>
-          <a className="botao" onClick={this.limpar}>Limpar</a>
+          <a className="botao" onClick={this.iniciar}>
+            {this.state.botao}
+          </a>
+          <a className="botao" onClick={this.limpar}>
+            Limpar
+          </a>
         </div>
-      
+
+        <img
+          src={require("../src/assets/andre_logonome.png")}
+          className="logonome"
+          alt="logonome"
+        />
       </div>
     );
   }
